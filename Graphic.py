ab5d4e6c9f433eab16a9c5016d7fdf23167728cd
Matthew Phillips <matt.phillips121@hotmail.com>
2017-09-12 09:35:05 +1000

    FIXED

--- Graphic.py
+++ Graphic.py
@@ -1,21 +1,11 @@
 from Rectangle import Rectangle, Circle
+#  https://github.com/MattPhillips1/comp1531-wk8
 
 
 class Graphic:
 
     def area_calculator(self, shape):
-        if isinstance(shape, Rectangle):
-            return shape._length * shape._width
-        elif isinstance(shape, Circle):
-            return 3.14 * shape._radius * shape._radius
-        else:
-            print("shape not defined")
+        return shape.area()
 
     def scale(self, shape, ratio):
-        if isinstance(shape, Rectangle):
-            shape._length *= ratio
-            shape._width *= ratio
-        elif isinstance(shape, Circle):
-            shape.radius *= ratio
-        else:
-            print("shape not defined")
+        shape.scale(ratio)
